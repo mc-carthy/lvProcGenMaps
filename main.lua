71150f4d1ffb837f4b1497f69918454ac269953d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-02-09 13:35:24 +0000

    Fix alive/dead cell status for celAut map

--- main.lua
+++ main.lua
@@ -13,7 +13,14 @@ end
 function love.draw()
     for x = 1, grid.xSize do
         for y = 1, grid.ySize do
-            love.graphics.setColor(grid[x][y], grid[x][y], grid[x][y])
+            if grid[x][y] == 0 then
+                love.graphics.setColor(0.25, 0.25, 0.25)
+            elseif grid[x][y] == 1 then
+                love.graphics.setColor(1, 0.75, 0.25)
+            else
+                love.graphics.setColor(1, 0, 1)
+            end
+            -- love.graphics.setColor(grid[x][y], grid[x][y], grid[x][y])
             love.graphics.rectangle('fill', (x - 1) * grid.cellSize, (y - 1) * grid.cellSize, grid.cellSize, grid.cellSize)
         end
     end
--- src/map/celAutMap.lua
+++ src/map/celAutMap.lua
@@ -1,7 +1,7 @@
 local CelAutMap = Class:extend()
 
 function CelAutMap:new(params)
-    self.cellSize = params.cellSize or 10
+    self.cellSize = params.cellSize or 5
     self.xSize = params.xSize or love.graphics.getWidth() / self.cellSize
     self.ySize = params.ySize or love.graphics.getHeight() / self.cellSize
     self.smoothingIterations = params.smoothingIterations or 2
@@ -27,9 +27,9 @@ function CelAutMap:initialiseGridValues()
     for x = 1, self.xSize do
         for y = 1, self.ySize do
             if love.math.random() > self.fillPercentage then
-                self[x][y] = 0
-            else
                 self[x][y] = 1
+            else
+                self[x][y] = 0
             end
         end
     end
@@ -42,7 +42,7 @@ function CelAutMap:smoothGrid()
             local neighbourCount = 0
             for dx = -1, 1 do
                 for dy = -1, 1 do
-                    if not (dx == 0 and dy == 0) and self[x + dx] and self[x + dx][y + dy] == 1 then
+                    if not (dx == 0 and dy == 0) and self[x + dx] and self[x + dx][y + dy] == 0 then
                         neighbourCount = neighbourCount + 1
                     end
 
@@ -55,17 +55,17 @@ function CelAutMap:smoothGrid()
             end
 
             if x <= mapBorderThickness or x > self.xSize - mapBorderThickness then
-                self[x][y] = 1
+                self[x][y] = 0
             end
 
             if y <= mapBorderThickness or y > self.ySize - mapBorderThickness then
-                self[x][y] = 1
+                self[x][y] = 0
             end
 
             if neighbourCount > 4 then
-                self[x][y] = 1
-            elseif neighbourCount < 4 then
                 self[x][y] = 0
+            elseif neighbourCount < 4 then
+                self[x][y] = 1
             end
         end
     end
