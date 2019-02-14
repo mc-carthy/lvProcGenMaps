fa73297eab44647915e2f92765de811122f2ffb0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-02-14 23:18:23 +0000

    Add octaves paramater to perlin noise map

--- src/map/perlinMap.lua
+++ src/map/perlinMap.lua
@@ -2,11 +2,12 @@ local PerlinMap = Class:extend()
 
 function PerlinMap:new(params)
     self.seed = params.seed or os.time()
+    self.numOctaves = 3
     love.math.setRandomSeed(self.seed)
-    self.cellSize = params.cellSize or 5
+    self.cellSize = params.cellSize or 1
     self.xSize = params.xSize or love.graphics.getWidth() / self.cellSize
     self.ySize = params.ySize or love.graphics.getHeight() / self.cellSize
-    self.scale = params.scale or 0.01
+    self.scale = params.scale or 0.004
     self.xOff, self.yOff = love.math.random(10000), love.math.random(10000)
     self:createGrid()
     self:initialiseGridValues(self.seed)
@@ -17,16 +18,17 @@ function PerlinMap:createGrid()
     for x = 1, self.xSize do
         self[x] = {}
         for y = 1, self.ySize do
-            self[x][y] = {}
+            self[x][y] = 0
         end
     end
 end
 
 function PerlinMap:initialiseGridValues(seed)
-    self.seed = seed
-    for x = 1, self.xSize do
-        for y = 1, self.ySize do
-            self[x][y] = love.math.noise((x * self.scale) + self.xOff, (y * self.scale) + self.yOff)
+    for o = 0, self.numOctaves - 1 do
+        for x = 1, self.xSize do
+            for y = 1, self.ySize do
+                self[x][y] = self[x][y] + (love.math.noise((x * (self.scale * (o + 1))) + self.xOff, (y * (self.scale * (o + 1))) + self.yOff)) * (0.5 ^ o)
+            end
         end
     end
 end
