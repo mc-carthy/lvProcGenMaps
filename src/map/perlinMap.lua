3bc9ecbee5831f15a34e76da4a8d6a9eca7f2ec8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-02-14 23:10:45 +0000

    Create normalise function for perlin noise map

--- src/map/perlinMap.lua
+++ src/map/perlinMap.lua
@@ -10,6 +10,7 @@ function PerlinMap:new(params)
     self.xOff, self.yOff = love.math.random(10000), love.math.random(10000)
     self:createGrid()
     self:initialiseGridValues(self.seed)
+    self:normaliseGridValues()
 end
 
 function PerlinMap:createGrid()
@@ -26,6 +27,22 @@ function PerlinMap:initialiseGridValues(seed)
     for x = 1, self.xSize do
         for y = 1, self.ySize do
             self[x][y] = love.math.noise((x * self.scale) + self.xOff, (y * self.scale) + self.yOff)
+        end
+    end
+end
+
+function PerlinMap:normaliseGridValues()
+    local min, max = math.huge, 0
+    for x = 1, self.xSize do
+        for y = 1, self.ySize do
+            if self[x][y] > max then max = self[x][y] end
+            if self[x][y] < min then min = self[x][y] end
+        end
+    end
+    
+    for x = 1, self.xSize do
+        for y = 1, self.ySize do
+            self[x][y] = (self[x][y] - min) / (max - min)
         end
     end
 end
